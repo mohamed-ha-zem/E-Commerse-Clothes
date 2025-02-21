27088913c0a966deb74581409439bb0d58f991ab
Mohamed Hazem <gggftyyfd@gmail.com>
2025-02-21 05:55:28 +0200

    edit index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import "./all.min.css"
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
 import UserProvider from './pages/website/Auth/context';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+  // <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+  <HashRouter>
     <UserProvider>
       <App />
     </UserProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
